614bbe441a805c8c2d3b1ae257a68596481c6e9e
BenBengler <77937801+BenBengler@users.noreply.github.com>
2021-01-31 13:09:11 +0100

    Update What.js

--- src/components/What.js
+++ src/components/What.js
@@ -11,19 +11,19 @@ export default () => {
           Ben Bengler
         </a>{" "}
         (music, live electronics) and Thomas Geissl (light, programming). It’s
-        driven by our shared vision for creating performances where the audience
+        driven by their shared vision for creating performances where the audience
         is in the centre of things, with sound and light unfolding around them.
       </p>
       <p>
-        Largely free from strictly timed sequencing, I create and evolve the
-        music via real-time audio manipulation and layered sound processing
-        techniques. The sound is analysed in real-time and Geissl uses the
+        Largely free from strictly timed sequencing, Bengler creates and evolves the
+        music via real-time audio manipulation and sound processing
+        techniques. The music is analysed in real-time and Geissl uses the
         derived data as a raw material, augmenting and manipulating it with a
-        custom software tool to creatively drive the the spatial light setup.
+        custom software tool to play the spatial light setup.
       </p>
 
       <p>
-        In this way, we try to conjure up an interconnected and fluid
+        In this way, Bengler/Geissl conjure up an interconnected and fluid
         choreography of sound and light unfolding around the audience.
       </p>
     </Section>
